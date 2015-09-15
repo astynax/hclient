47a01b275962d58f113cade07104adcd9f48d0bf
Aleksey <astynax84@gmail.com>
2015-09-15 18:07:03 +0300

    Main.hs: some small edits

--- src/Main.hs
+++ src/Main.hs
@@ -5,13 +5,13 @@ import           Types
 
 
 main :: IO ()
-main = runGUI "echo" (Just "Echo service ready!\n\n") (1 :: Int) echo
+main = runGUI "echo" (Just "Echo service is ready!\n\n") (1 :: Int) echo
 
 
 echo :: (Show a, Num a) => Controller a
-echo s msg =
-  let (f, action) = case msg of
+echo state msg =
+  let (update, action) = case msg of
         ":q" -> (id,      Quit)
         ":r" -> (const 1, Clear)
-        _    -> ((+ 1),   Write $ show s ++ ": " ++ msg ++ "\n")
-  in  return (f s, Just "", [action])
+        _    -> ((+ 1),   Write $ show state ++ ": " ++ msg ++ "\n")
+  in  return (update state, Just "", [action])
