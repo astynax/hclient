08257c95476c834689818b234f6c29f65d06d26c
Aleksey <astynax84@gmail.com>
2015-09-24 13:16:23 +0300

    TUI refactored

--- src/TUI.hs
+++ src/TUI.hs
@@ -2,47 +2,62 @@
 
 module TUI (runTUI) where
 
-import           Control.Monad (void, foldM)
-import           Types
+import           System.IO       (hFlush, stdout)
+import           System.IO.Error (catchIOError, isEOFError)
+
+import           Core
 
 
-type TUIOutput = Output ([String] -> IO [String])
-type TUIState a = (Bool, [String], a)
+data TUIState  a = Run { buffer   :: String
+                       , ctlState :: a
+                       }
+                 | Stop a -- final state of Controller
 
 
-runTUI :: [Action a] -> UI a
-runTUI setup ctl = do
-  state <- initialize ctl
+tuiOutput :: Output (TUIState a -> TUIState a)
+tuiOutput = Output { writeTo     = \msg s -> s { buffer = buffer s ++ msg }
+                   , clearOutput = \    s -> s { buffer = [] }
+                   , scrollTo    = \_   s -> s -- TUI can't scroll
+                   }
 
-  let out = Output { writeTo     = \x s -> return $ s ++ [x]
-                   , clearOutput = \_   -> return []
-                   , scrollTo    = \_ s -> return s }
 
-  void $ perform out (False, [], state) setup >>= loop ctl out
-
+runTUI :: Show a => [Action a] -> UI a
+runTUI setup ctl =
+  loop . perform setup . Run "" =<< initialize ctl
   where
-    loop :: Controller IO a -> TUIOutput -> TUIState a -> IO (TUIState a)
-    loop c _   state@(True, _, a) = finalize c a >> return state
-    loop c out state@(_,    s, a) =
-      mapM_ putStrLn s
-      >> getLine
-      >>= communicate c a
-      >>= perform out state
-      >>= loop c out
+    loop (Stop s) = finalize ctl s >> putStrLn "\nBye!"
+    loop s        = do
+      putStr (buffer s)
 
-    perform out = foldM (apply out)
+      let s' = s { buffer = [] }
 
-    apply :: Output (a -> IO a) -> (Bool, a, b) -> Action b -> IO (Bool, a, b)
-    apply _ state@(True, _, _) _    = return state
-    apply _       (_,    s, b) Quit = return (True, s, b)
-    apply out     (_,    s, b) act  =
-      case act of
-        Write msg   -> with s b $ writeTo out msg
-        ClearOutput -> with s b $ clearOutput out
-        SetInput _  -> keep s b
-        SetState b' -> keep s b'
-        ScrollTo t  -> with s b $ out `scrollTo` t
-        _           -> error "This case should't be reached!"
+      mbInput <- getInput
 
-    with s b f = fmap (\s' -> (False, s', b)) (f s)
-    keep = (return .) . (\s b -> (False, s, b))
+      loop =<< maybe
+        (return $ Stop $ ctlState s')
+        (\input -> do
+            actions <- communicate ctl (ctlState s') input
+            return $ perform actions s'
+        )
+        mbInput
+
+    perform = flip (foldl apply)
+      where
+        apply s@(Stop _) _    = s
+        apply s          Quit = Stop (ctlState s)
+        apply s          act  =
+          case act of
+            Write msg   -> writeTo tuiOutput msg
+            ClearOutput -> clearOutput tuiOutput
+            ScrollTo t  -> tuiOutput `scrollTo` t
+            SetState x  -> \s' -> s' { ctlState = x }
+            SetInput _  -> id -- TUI can't set the input
+            Quit        -> error "This case should't be reached!"
+          $ s
+
+getInput :: IO (Maybe String)
+getInput = catchIOError
+           (putStr "> " >> hFlush stdout >> Just <$> getLine)
+           (\e -> if isEOFError e
+                  then return Nothing
+                  else ioError e)
